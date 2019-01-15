87406acb168c0d4e3f62fdeefed679005b3bec8d
Yang Zeng <zengyang@vmware.com>
2019-01-15 11:32:38 -0800

    for Xue

--- avrodefs/Anomaly.py
+++ avrodefs/Anomaly.py
@@ -5,7 +5,7 @@ import copy
 
 
 class Anomaly(object):
-    def __init__(self, customer, start_ms, end_ms, query_hash, param_hash, chart_hash,
+    def __init__(self, customer, start_ms, end_ms, query_hash, param_hash, chart_hash, chart_link,
                  dashboard_id, section, row, col, model="N/A", image_link="N/A",
                  query_params={}):
         self.customer = customer
@@ -13,6 +13,7 @@ class Anomaly(object):
         self.endMs = end_ms
         self.queryHash = query_hash
         self.paramHash = param_hash
+        self.chart_link = chart_link
         self.chartHash = chart_hash
         self.dashboardId = dashboard_id
         self.section = section
