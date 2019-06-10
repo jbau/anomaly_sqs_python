84d60ecfe33dff69a5e0cb01d51a90d91f9f7604
Xue Tong <xtong@vmware.com>
2019-06-10 14:39:22 -0700

    Add metricsUsed and hostsUsed, change example code

--- SQSExample.py
+++ SQSExample.py
@@ -1,10 +1,10 @@
 from avrodefs.Anomaly import Anomaly
 from sqs_writer.SQSAnomalyReporter import SQSAnomalyReporter
 
-queue_name = "anomaly-reporting-dev"
+queue_name = "anomaly-reporting-dev-local"
 region_name = "us-west-2"
 reporter = SQSAnomalyReporter(queue_name, region_name=region_name)
-anomaly = Anomaly(customer="mycustomer", start_ms=1000, end_ms=1001, query_hash="query_hash_my_test", param_hash="ph",
-                  chart_hash="ch_ch", dashboard_id="dbid", section=0, row=0, col=0, model="model123",
-                  image_link="http://link")
+anomaly = Anomaly(customer="xtong_test", start_ms=5000, end_ms=5001, query_hash="query_hash_xtong_test", param_hash="ph",
+                  chart_hash="ch_ch", dashboard_id="dbid", section=0, row=0, col=0, model="model235",
+                  image_link="https://wavefront-ml-dev.s3-us-west-2.amazonaws.com/training/local/collector/2019/02/03/24H/RepeatCycle/T0127Z_training_section_0_row_0_chart_0_1549067232_1549153632_params_0-scaled.png", metrics_used=["m1", "m2"], hosts_used=["h1", "h2"])
 reporter.reportAnomaly(anomaly)
--- avrodefs/Anomaly.py
+++ avrodefs/Anomaly.py
@@ -7,7 +7,7 @@ import copy
 class Anomaly(object):
     def __init__(self, customer, start_ms, end_ms, query_hash, param_hash, chart_hash,
                  dashboard_id, section, row, col, model="N/A", image_link="N/A", chart_link="N/A",
-                 query_params={}):
+                 query_params={}, metrics_used=[], hosts_used=[]):
         self.customer = customer
         self.startMs = start_ms
         self.endMs = end_ms
@@ -24,3 +24,5 @@ class Anomaly(object):
         self.queryParams = copy.copy(query_params)
         self.updatedMs = 0  # placeholder values that can't be set
         self.originalStripes = []  # placeholder values that can't be set
+        self.metricsUsed = metrics_used
+        self.hostsUsed = hosts_used
